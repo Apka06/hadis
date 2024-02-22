dfb0bae0e0990432573ce0d5e0b1a498dfc22730
Apka06 <93733936+Apka06@users.noreply.github.com>
2024-02-22 17:29:18 +0300

    favorite model

--- hadis/Hadis_Deryasi/models.py
+++ hadis/Hadis_Deryasi/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Favorites(models.Model):
+    number = models.IntegerField(unique = True)
+    content = models.CharField(max_length = 255)
+    owner = models.ForeignKey(User, on_delete= models.CASCADE)
+
+    def __str__(self) -> str:
+        return str(self.owner) + " 's " + str(self.number)
