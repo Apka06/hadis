b752cc4eb7911f2c242eab8a405815f162233419
Said ARSLAN <muhammedsaidarslan@gmail.com>
2024-02-25 19:23:16 +0300

    accounts

--- hadis/hadis/settings.py
+++ hadis/hadis/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Hadis_Deryasi',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -117,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+LOGIN_REDIRECT_URL ='home'
+LOGOUT_REDIRECT_URL = 'home'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- hadis/hadis/urls.py
+++ hadis/hadis/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('Hadis_Deryasi.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('',include('accounts.urls')),
 ]
